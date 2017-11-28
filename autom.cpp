0a52e7da58e3d6bd37b7bc01cea0c7ec864a1160
laucacot <cacot.laura@gmail.com>
2017-11-28 09:38:28 -0500

    code avec f5 est complique a faire tourner car le jacobien est dur a calucle essaie de nouveau code avec fortran

--- autom.cpp
+++ autom.cpp
@@ -25,37 +25,50 @@ typedef boost::numeric::ublas::vector< value_type > state_type;
 typedef boost::numeric::ublas::matrix< value_type > matrix_type;
 typedef rosenbrock4< value_type > stepper_type;
 
-// constants
+// constantes
 const value_type pression = 0.1/760; //atm soit 0.1 torr et 13 pascal 
-const value_type L = 3.e-2; //distance netre deux plaques en m
+const value_type L = 3.e-2; //distance entre deux plaques en m
 const value_type pi = M_PI;
 const value_type diff = pow((pi/L), 2.);//facteur pour la diffusion 
 const value_type n_Ar =  pression*2.69e25; //densite d'argon en m-3
 const value_type n_SiH4_ini = n_Ar/30.; //densite de SiH4 initiale
-const int Nbr_espece=21;
-const value_type DP = 1.e23;//eV/s.m3 puissance totale du systeme par unite de volume imposee
-const float C=1.e20;// m-3/s taux d'injection du SiH4 dans le réacteur
+const int Nbr_espece=24;
+//const value_type DP = 1.e23;//eV/s.m3 puissance totale du systeme par unite de volume imposee
+const float C=1.35e21;// m-3/s taux d'injection du SiH4 dans le réacteur
 const value_type Tg =0.02758 ; //eV soit 320 K
 const float D=1.;// m-3/s taux d'injection du SiH4 dans le réacteur
 
+const value_type e0=1.6022e-19; //Coulomb valeur de la charge elementaire
+const value_type eps0=8.8542e-12; //s2.C2/kg/m3 permittivite du vide
+const value_type cVfl=e0/(4.*pi*eps0); //champ electrique ??
+const value_type V=100.; //V voltage applique
 
+const value_type rNP=1.e-9; //m rayon des NP
+const value_type qNP=0.; //C charge initiale des NP
+const value_type nNP=2.e15; //m-3 densite initiale des NP
+
+const value_type vSi=(4./3.)*pi*pow(1.36e-10,3); //m3 volume d'un atome de Si
+const value_type tauxinj_ev=C/n_SiH4_ini; //taux d'injection et d'evacuation
 
 //calcul des diffusions
 state_type DL(Nbr_espece, 0.0); //vecteur de diffusion libre en m2/s
 state_type mu(Nbr_espece, 0.0); //vecteur de mobilite en m2/(V.s)
 state_type DA(Nbr_espece, 0.0); //vecteur de diffusion ambipolaire en m2/s
+ 
+//calcul collage
+state_type coll(Nbr_espece,0.0); //vecteur collage
+state_type vth(Nbr_espece,0.0); //vecteur vitesse thermique
 
-
-const int Nbr_K=47;
+const int Nbr_K=47; //nombre d'equation dans le fichier
 int jmax=Nbr_K;
-int imax=9;
-double **Tab;
+int imax=9; //nombre de colonnes
+double **Tab; //creation du tableau
 
-value_type p1,p2,g1,g2,g3,g4,Tp,Tx,Tj;
+value_type p1,p2,g1,g2,g3,g4,Tp,Tx,Tj,Fe,facte,factp,factn;
 state_type Kt(jmax, 0.0);
 
 
-value_type k (int ind, value_type Tp) //K58 SiH3- + SiH3+ ->  Si2H4 + H2
+value_type k (int ind, value_type Tp) //fonction pour calculer les K en faisant varier Te dans la bissection
 {
     value_type K;
     K= Tab[6][ind]*pow(Tp,Tab[7][ind])*exp(-Tab[8][ind]/Tp);
@@ -63,7 +76,7 @@ value_type k (int ind, value_type Tp) //K58 SiH3- + SiH3+ ->  Si2H4 + H2
 }
 
 
-struct Condition
+struct Condition //condition sur la bissection
 {
   value_type tol=1.e-9;
   bool operator() (value_type min, value_type max)  {
@@ -73,7 +86,7 @@ struct Condition
 
 
 
-struct nsystem
+struct nsystem //structure qui vient calculer les equations differentielles
 {
   void operator()(const state_type &n, state_type &dndt, const value_type &t)
   {
@@ -81,42 +94,74 @@ struct nsystem
 
     /*0=e, 1=Armet, 2=SiH3-, 3=SiH2-, 4=SiH3+, 5=SiH4, 6=SiH3,
     7=H, 8=SiH2, 9=H2, 10=H2+, 11=Si2H5, 12=Si2H2, 13=Si2H4-,
-    14=Si2H6, 15=Si2H3-, 16=Si2H5-, 17=SiH-, 18=SiH, 19=Si, 20=Arp, 21=NP*/
+    14=Si2H6, 15=Si2H3-, 16=Si2H5-, 17=SiH-, 18=SiH, 19=Si, 20=Arp, 
+    21=rNP, 22=qNP, 23=nNP  */
 
- value_type   dSi= n[2] + n[3] + n[4] + n[5] + n[6] + n[8] + 2.*n[11] 
+
+ value_type dpp= n[4]+n[10]+n[20];
+ value_type dnn= n[0] + n[2] + n[3] + n[13] + n[15] + n[16] + n[17];
+ 
+ value_type ratep = tauxinj_ev *dnn/dpp; //pour que les charges sortent par paires = et -
+
+ value_type Vfl=cVfl*n[22]/n[21];//V potentiel flottant e*qNP / 4 pi eps0 rNP
+cerr<<Vfl<<endl;
+ value_type sNP=pi*pow(n[21],2); //surface de la NP
+//qNP<0   else //qNP>0
+ if (Vfl<0.){ facte=sNP*exp(Vfl/Te); 
+	      factn=sNP*exp(Vfl/Tg);
+	      factp=sNP*(1.-Vfl/Tg);}
+ else 	    { facte=sNP*(1.+Vfl/Te); 
+	      factn=sNP*(1.+Vfl/Tg);
+	      factp=sNP*exp(-Vfl/Tg);}
+
+
+ value_type dSi= n[2] + n[3] + n[4] + n[5] + n[6] + n[8] + 2.*n[11] 
       + 2.*n[12] + 2.*n[13] + 2.*n[14] + 2.*n[15] + 2.*n[16] + n[17] 
       + n[18] + n[19]; //somme de tous les atomes de si 
 
-//cerr<<dSi<<endl;
+//chargement des NP
+value_type ffe=facte*vth[0]*n[0];
+value_type ffn2=factn*vth[2]*n[2];
+value_type ffn3=factn*vth[3]*n[3];
+value_type ffp4=factp*vth[4]*n[4];
+value_type ffp10=factp*vth[10]*n[10];
+value_type ffn13=factn*vth[13]*n[13];
+value_type ffn15=factn*vth[15]*n[15];
+value_type ffn16=factn*vth[16]*n[16];
+value_type ffn17=factn*vth[17]*n[17];
+value_type ffp20=factp*vth[20]*n[20];
+
+value_type ccol=nNP*4.*sNP/vSi; //4*sNP=surface de la NP
 
 for (int k=0;k<Nbr_espece;k++)
 {
-dndt[k]=0;
+dndt[k]=0; //on initialise les equations a zero 
 }
 
 
 for (int j=0;j<jmax;j++)
 {
 
- p1=Tab[0][j];
- p2=Tab[1][j];
- g1=Tab[2][j];
- g2=Tab[3][j];
- g3=Tab[4][j];
- g4=Tab[5][j]; 
- Tp=(p2==0 or g3==0)?Te:Tg;
+ p1=Tab[0][j]; //perte 1 (reactif)
+ p2=Tab[1][j]; //perte 2 (reactif)
+ g1=Tab[2][j]; //gain 1 (produit)
+ g2=Tab[3][j]; //gain 2 (produit)
+ g3=Tab[4][j]; //gain 3 (produit)
+ g4=Tab[5][j]; //gain 4 (produit)
+ 
+ Tp=(p2==0 or g3==0)?Te:Tg; //on defini si la temperature vaut Tg ou Te en fonction de si les electrons interviennent dans la reaction  
+ Kt[j]={Tab[6][j]*pow(Tp,Tab[7][j])*exp(-Tab[8][j]/Tp)}; //coefficient de la loi d'arrhenius
 
- Kt[j]={Tab[6][j]*pow(Tp,Tab[7][j])*exp(-Tab[8][j]/Tp)};
-
-if (p1==200)
+//on exprime les therme de gain ou pertes en fonction des especes qui reagissent
+if (p1==200) //un des deux reactifs est de l'argon (densite constante)
 {
 Tx=n_Ar*n[p2]*Kt[j];
 }
-else if (p2==100)
+else if (p2==100) // il n'y a qu'un reactif
 {
 Tx=n[p1]*Kt[j];
 }
-else
+else //cas normal de deux reactifs qui peuvent etre egaux
 {
 Tx=n[p1]*n[p2]*Kt[j];
 }
@@ -131,13 +176,58 @@ if(g4!=100) {dndt[g4]=dndt[g4]+Tx;}
 
 }
 
+//introduction de la diffusion
 for (int a=0; a<Nbr_espece;a++)
-{if (a!=0 or a!=1 or a!=20) {dndt[a]=dndt[a]-C*n[a]/dSi;}
-if (a==0 or a==1 or a==4 or a==10 or a==20) {dndt[a]=dndt[a]-DA[a]*n[a];}}
+{
+if (a==0 or a==1 or a==4 or a==10 or a==20) {dndt[a]=dndt[a]-DA[a]*n[a];} //diffusion
+if (a!=4 or a!=10 or a<20){dndt[a]=dndt[a]-tauxinj_ev*n[a];}//pompe sur les especes non +
+if (a==4 or a==10 or a==20){dndt[a]=dndt[a]-ratep*n[a];}//pompe sur les especes +
+}  
+dndt[0]=dndt[0]-ffe*nNP;
+dndt[1]=dndt[1]-sNP*vth[1]*nNP*n[1];
+dndt[2]=dndt[2]-ffn2*nNP;
+dndt[3]=dndt[3]-ffn3*nNP;
+dndt[4]=dndt[4]-ffp4*nNP;
+dndt[5]=dndt[5]-ccol*coll[5]*n[5]
+		+C;//insertion de SiH4 dans le reacteur
+dndt[6]=dndt[6]-ccol*coll[6]*n[6]    
+		+DA[4]*n[4];
+dndt[7]=dndt[7]+(ffn2+ffp4+ffn15+ffn16+ffn17)*nNP
+		+ccol*(coll[6]*n[6]+coll[11]*n[11]+coll[18]*n[18])
+		+0.3*DA[4]*n[4];//SiH3+ + e -> Si + H + H2 sur paroi
+dndt[8]=dndt[8]-ccol*coll[8]*n[8]
+		+DA[4]*n[4];//SiH3+ + e -> SiH2 + H sur paroi
+dndt[9]=dndt[9]+DA[10]*n[10]// H2+ + e -> H2 sur paroi 
+		+DA[4]*n[4] //SiH3+ + e -> SiH + H2, Si + H + H2 sur paroi
+		+(ffn2+ffn3+ffp4+ffp10+2.*ffn13+ffn15+2.*ffn16)*nNP
+		+ccol*(2.*coll[5]*n[5]+coll[6]*n[6]+coll[9]*n[9]+2.*coll[11]*n[11]
+			+coll[12]*n[12]+3.*coll[15]*n[15]);
+dndt[10]=dndt[10]-ffp10*nNP;
+dndt[11]=dndt[11]-ccol*coll[11]*n[11];
+dndt[12]=dndt[12]-ccol*coll[12]*n[12];
+dndt[13]=dndt[13]-ffn13*nNP;
+dndt[14]=dndt[14]-ccol*coll[14]*n[14];
+dndt[15]=dndt[15]-ffn15*nNP;
+dndt[16]=dndt[16]-ffn16*nNP;
+dndt[17]=dndt[17]-ffn17*nNP;
+dndt[18]=dndt[18]+DA[4]*n[4]//SiH3+ + e ->  SiH + H2 sur paroi
+		-ccol*coll[18]*n[18];
+dndt[19]=dndt[19]-ccol*coll[19]*n[19]
+		+0.3*DA[4]*n[4];//SiH3+ + e ->  Si + H + H2 sur paroi
+dndt[20]=dndt[20]-ffp20*nNP;
 
-dndt[5]=dndt[5]+C;
-dndt[6]=dndt[6]+DA[4]*n[4];
-dndt[9]=dndt[9]+DA[10]*n[10];
+//Collage dR/dt sur les NP:
+dndt[21]=dndt[21] +coll[5]*n[5] +coll[6]*n[6] +coll[8]*n[8]
+		+2.*coll[11]*n[11] +2.*coll[12]*n[12]
+		+2.*coll[14]*n[14] +coll[18]*n[18] +coll[19]*n[19]
+		+(ffn2 +ffn3 +2.*ffn13 +2.*ffn15 +2.*ffn16 +ffn17 +ffp4)*vSi/sNP;
+
+// Charge des NP par OML: 
+dndt[22]=dndt[22]-ffe -ffn2 -ffn3 -ffn13 -ffn15 -ffn16 -ffn17
+		+ffp4 +ffp10 +ffp20;
+
+// Densite des NP:
+dndt[23]=dndt[23] + dndt[13] +dndt[17];
 
   }
 
@@ -155,7 +245,7 @@ value_type   dSi= n[2] + n[3] + n[4] + n[5] + n[6] + n[8] + 2.*n[11]
       + 2.*n[12] + 2.*n[13] + 2.*n[14] + 2.*n[15] + 2.*n[16] + n[17] 
       + n[18] + n[19]; //somme de tous les atomes de si 
 
-for (int h=0;h<Nbr_espece;h++)
+for (int h=0;h<Nbr_espece;h++) //initialisation du jacobien a zero 
 
 {
 for (int p=0;p<Nbr_espece;p++)
@@ -181,11 +271,11 @@ for (int j=0;j<jmax;j++)
  Tp=(p2==0 or g3==0)?Te:Tg;
 
  Kt[j]={Tab[6][j]*pow(Tp,Tab[7][j])*exp(-Tab[8][j]/Tp)};
-//cerr<<j<<endl;
+
 for (int k=0;k<Nbr_espece;k++)
 {
 
-if (p1==200 and p2==k and p1!=p2)
+if (p1==200 and p2==k and p1!=p2) //cas d'une reaction faisant intervenir de l'argon , on derive par rapport a p2
 	{Tj=n_Ar*Kt[j];
 	jacobi(p2,k)=jacobi(p2,k)-Tj;
 	if (g1!=200) {jacobi(g1,k)=jacobi(g1,k)+Tj;}
@@ -193,7 +283,7 @@ if (p1==200 and p2==k and p1!=p2)
 	if (g3!=100) {jacobi(g3,k)=jacobi(g3,k)+Tj;}
 	if (g4!=100) {jacobi(g4,k)=jacobi(g4,k)+Tj;}
 	}
-if (p1!=200 and p2==k and p1!=p2) 
+if (p1!=200 and p2==k and p1!=p2) //cas d'une reaction sans argon , on derive par rapport a p2
 	{Tj=n[p1]*Kt[j];
 	jacobi(p1,k)=jacobi(p1,k)-Tj;
 	jacobi(p2,k)=jacobi(p2,k)-Tj;
@@ -202,7 +292,7 @@ if (p1!=200 and p2==k and p1!=p2)
 	if (g3!=100) {jacobi(g3,k)=jacobi(g3,k)+Tj;}
 	if (g4!=100) {jacobi(g4,k)=jacobi(g4,k)+Tj;}
 	}
-if (p2==100 and p1==k and p1!=p2) 
+if (p2==100 and p1==k and p1!=p2) //cas ou il y a qu'un reactif p1, on derive par rapport a p1
 	{Tj=Kt[j];
 	jacobi(p1,k)=jacobi(p1,k)-Tj;
 	if (g1!=200) {jacobi(g1,k)=jacobi(g1,k)+Tj;}
@@ -210,7 +300,7 @@ if (p2==100 and p1==k and p1!=p2)
 	if (g3!=100) {jacobi(g3,k)=jacobi(g3,k)+Tj;}
 	if (g4!=100) {jacobi(g4,k)=jacobi(g4,k)+Tj;}
 	}
-if (p1==k and p1!=p2 and p2!=100 ) 
+if (p1==k and p1!=p2 and p2!=100 ) //cas d'une reaction sans argon , on derive par rapport a p1
 	{Tj=n[p2]*Kt[j];
 	jacobi(p1,k)=jacobi(p1,k)-Tj;
 	jacobi(p2,k)=jacobi(p2,k)-Tj;
@@ -219,7 +309,7 @@ if (p1==k and p1!=p2 and p2!=100 )
 	if (g3!=100) {jacobi(g3,k)=jacobi(g3,k)+Tj;}
 	if (g4!=100) {jacobi(g4,k)=jacobi(g4,k)+Tj;}
 	}
-if (p1==p2) 
+if (p1==p2) //cas ou les deux reactifs sont egaux
 	{Tj=2*n[p1]*Kt[j];
 	if (p1!=200) {jacobi(p1,k)=jacobi(p1,k)-Tj;}
 	if (p2!=100) {jacobi(p2,k)=jacobi(p2,k)-Tj;}
@@ -233,12 +323,14 @@ if (p1==p2)
 
 }
 
+//diffusion dans le jacobien 
 for (int a=0; a<Nbr_espece;a++)
-{if (a!=0 or a!=1 or a!=20) {jacobi(a,a)=jacobi(a,a)-C/dSi;}
-if (a==0 or a==1 or a==4 or a==10 or a==20) {jacobi(a,a)=jacobi(a,a)-DA[a];}}
+{if (a!=0 or a!=1 or a!=20) {jacobi(a,a)=jacobi(a,a)-C/dSi;} //perte dans la pompe
+if (a==0 or a==1 or a==4 or a==10 or a==20) {jacobi(a,a)=jacobi(a,a)-DA[a];}} //diffusion 
 
-jacobi(6,4)=jacobi(6,4)+DA[4];
-jacobi(9,10)=jacobi(9,10)+DA[10];
+jacobi(9,4)=jacobi(9,4)+DA[4];//SiH3+ + e ->  SiH + H2 sur paroi
+jacobi(9,10)=jacobi(9,10)+DA[10];// H2+ + e -> H2 sur paroi
+jacobi(18,4)=jacobi(18,4)+DA[4];// SiH3+ + e ->  SiH + H2 sur paroi
 
     dfdt( 0 ) = 0.0;
     dfdt( 1 ) = 0.0;
@@ -261,31 +353,46 @@ jacobi(9,10)=jacobi(9,10)+DA[10];
     dfdt( 18 ) = 0.0;
     dfdt( 19 ) = 0.0;
     dfdt( 20 ) = 0.0;
+    dfdt( 21 ) = 0.0;
+    dfdt( 22 ) = 0.0;
+    dfdt( 23 ) = 0.0;
   }
 //stop modif
   value_type Te;
 };
 
-
+//calcul de la temperature a partir de la puissance dans le reacteur 
 struct etemperature
 {
   value_type operator()(value_type const& Te)
   
-{   return 
+{ value_type DP=0.5*DL[0]*pow((V/L),2.);  //eV.eV/s puissance par unite de volume/ne*Te
+  value_type Vfl=cVfl*n[22]/n[21];//V potentiel flottant e*qNP / 4 pi eps0 rNP
+  value_type sNP=pi*pow(n[21],2); //surface de la NP
+  if (Vfl<=0.){ Fe=sNP*vth[0]*exp(Vfl/Te);}
+  else { Fe=sNP*vth[0]*(1.-Vfl/Te);}
+
+ return 
 	-DP/n[0]
-    +k(0,Te)*n_Ar*16.14 +k(1,Te)*n_Ar*12.31+ k(2,Te)*n[1]*5.39
-    -k(3,Tg)*n[1]*n[1]*8.48
-    -k(4,Te)*n[1]*12.31
-    +k(5,Te)*n[5]*10.68 +k(6,Te)*n[5]*10.68 + k(7,Te)*n[5]*8.29 + k(8,Te)*n[5]*8.29
-    +k(9,Te)*n[5]*24.1 + k(10,Te)*n[6]*1.94 + k(11,Te)*n[6]*1.30 + k(12,Te)*n[2]*1.16
-    +k(13,Te)*n[3]*1.16 + k(14,Te)*n[8]*1.5*Te + k(15,Te)*n[9]*10.09
-    +k(16,Te)*n[9]*16.05 +k(42,Te)*n[6]*1.5*Te +k(43,Te)*n[18]*1.5*Te
-    +k(44,Te)*n[17]*1.25;
+	-DP/Te
+    	+k(0,Te)*n_Ar*15.76 +k(1,Te)*n_Ar*11.76+ k(2,Te)*n[1]*4.
+    	-k(4,Te)*n[1]*11.76
+    	+k(5,Te)*n[5]*10.68 +k(6,Te)*n[5]*10.68
+	+k(7,Te)*n[5]*8.29 + k(8,Te)*n[5]*8.29
+    	+k(9,Te)*n[5]*12
+	+k(10,Te)*n[6]*1.94 + k(11,Te)*n[6]*1.30 + k(12,Te)*n[2]*1.16
+    	+k(13,Te)*n[3]*1.16 + k(14,Te)*n[8]*1.5*Te + k(15,Te)*n[9]*10.09
+    	+k(16,Te)*n[9]*16.05 +k(42,Te)*n[6]*1.5*Te +k(43,Te)*n[18]*1.5*Te
+    	+k(44,Te)*n[17]*1.25
+	+DA[0]*1.5*Te /*perte sur les parois*/
+	+Fe*nNP*1.5*Te /*pertes sur les NP*/
+	+tauxinj_ev*1.5*Te /*perte taux injection evacuation*/;
   }
 
   state_type n;
 };
 
+//ecriture des densite dans un fichier 
 void write_density( const value_type t, const value_type Te, const state_type &n)
 {
   cout << t  << '\t' <<Te <<'\t' << n[0] << '\t' << n[1] << '\t'
@@ -299,7 +406,8 @@ void write_density( const value_type t, const value_type Te, const state_type &n
 
 int main(int argc, char **argv)
 {
-  
+
+//lecture dans le fichier contenant les reactions et les coefficients pour arrhenius  
 ifstream fichier_k ("/home/cacot/Documents/CodeAutom/fichagarwal.dat");
 
 Tab = new double*[imax];
@@ -334,24 +442,65 @@ else
  //cerr << Tab[0][jmax-1]<<endl;
 
 
-value_type Te=0.7;
+value_type Te=3.;//valeur initiale de la temperature
+
+//legende
 cout <<"t"<<'\t'<<"Te"<<'\t'<<"e"<<'\t'<<"Armet"<<'\t'<< "SiH3m"<<'\t'
                << "SiH2"<<'\t'<< "SiH3p"<<'\t'<< "SiH4"<<'\t'<< "SiH3"<<'\t'
                <<"H"<<'\t'<< "SiH2"<<'\t'<< "H2"<<'\t'<< "H2p"<<'\t'<< "Si2H5"
                <<'\t'<< "Si2H2"<<'\t'<<"Si2H4m"<<'\t'<<"Si2H6"<<'\t'<< "Si2H3m"
                <<'\t'<< "Si2H5m"<<'\t'<< "SiHm"<<'\t'<<"SiH"<<'\t'<< "Si"<<'\t'
                << "Arp"<<'\t'<<"NP"<<endl;
-//clock_t t1,t2;
 
 
-  // Density vectors and initial condition
-  state_type n_ini(Nbr_espece, 0.0); // initial conditions
+
+  // vecteur de densite et conditions initiales
+  state_type n_ini(Nbr_espece, 0.0); 
   n_ini[0] = 1.e16;
-  n_ini[1] = 1.e16;  // initial conditions
+  n_ini[1] = 1.e16;  
   n_ini[5] = n_SiH4_ini;
-  n_ini[20] =1.e16;
-  n_ini[4] = n_ini[0]-n_ini[20];
+  n_ini[20] =n_ini[0]-n_ini[4];
+  n_ini[21]=rNP;
+  n_ini[23]=qNP;
 
+//determination du collage sur les NP
+state_type coll(Nbr_espece,0.0); //vecteur collage
+state_type vth(Nbr_espece,0.0); //vecteur vitesse thermique
+
+value_type cvth=1.56e4*sqrt(Tg); //NRL * sqrt (8/pi)
+
+	vth[0]=6.69e5*sqrt(Te);
+	vth[1]=cvth/sqrt(40.);
+	vth[2]=cvth/sqrt(31.);
+	vth[3]= cvth/sqrt(30.);
+	vth[4]= cvth/sqrt(33.);
+	vth[5]= cvth/sqrt(34.);
+	vth[6]= cvth/sqrt(33.);
+	vth[7]= cvth/sqrt(1.);
+	vth[8]= cvth/sqrt(30.);
+	vth[9]= cvth/sqrt(2.);
+	vth[10]= cvth/sqrt(2.);
+	vth[11]= cvth/sqrt(61.);
+	vth[12]= cvth/sqrt(58.);
+	vth[13]= cvth/sqrt(60.);
+	vth[14]= cvth/sqrt(62.);
+	vth[15]= cvth/sqrt(59.);
+	vth[16]= cvth/sqrt(61.);
+	vth[17]= cvth/sqrt(29.);
+	vth[18]= cvth/sqrt(29.);
+	vth[19]= cvth/sqrt(28.);
+	vth[20]= cvth/sqrt(40.);
+
+	coll[5]= 1.e-5*vSi*vth[5];    // m4/s  coefficients de collage de Le Picard
+	coll[6]= 0.045*vSi*vth[6];
+	coll[8]= 0.8*vSi*vth[8];
+	coll[11]= 1.e-5*vSi*vth[11]; // pas dans Le Picard
+	coll[12]= 1.e-5*vSi*vth[12] ;// pas dans Le Picard
+	coll[14]= 1.e-5*vSi*vth[14]; 
+	coll[18]= 0.95*vSi*vth[18];
+	coll[19]= 1.0*vSi*vth[19]; // pas dans Le Picard
+
+//diffusion
 state_type DL(Nbr_espece, 0.0); //vecteur de diffusion libre en m2/s
 state_type mu(Nbr_espece, 0.0); //vecteur de mobilite en m2/(V.s)
 state_type DA(Nbr_espece, 0.0); //vecteur de diffusion ambipolaire en m2/s
@@ -383,47 +532,38 @@ DL[20]=4.e-3/(pression*760.); //valeur de benjamin
 mu[20]= DL[20]/Tg;
 
 
-// Time variables
+// variable du temps
   value_type t = 0.0;
   value_type dt = 1.0e-8;
   value_type Tmax = 20.e-3;
   value_type NT = Tmax/dt;
 
-  // Root finding variables
+  // variable pour la bissection 
   value_type min = Tg;
   value_type max = 10.0;
   boost::uintmax_t max_iter = 500;
   eps_tolerance<value_type> tol(30);
 
-  state_type n_new(Nbr_espece, 0.0);  // first step same as initial conditions
+  state_type n_new(Nbr_espece, 0.0);  // initialisation du vecteur densite 
   n_new = n_ini;
   state_type n_err(Nbr_espece, 0.0); //error
 
-  // declare the functor etemperature
+  // declare la fonction etemperature 
   etemperature etemp;
-  // assign initial values to functor etemp
+  //assigne les valeur a la fonction etemp
   etemp.n = n_ini;
 
-  //cerr << "\n[ii] Electrons  = " << etemp.n[0] << endl;
-  //cerr << "\n[ii] Metastables  = " << etemp.n[1] << endl;
-//   cout << "\n[ii] n  = " << etemp.n[2] << endl;
-
-//t1=clock();
-  // Find Te first calculation
+  // premier calcul de Te
   pair<value_type, value_type> pair_Te =\
                 toms748_solve(etemp, min, max, tol, max_iter);
 
   Te = pair_Te.first;
-  cerr << "\n[ii] Initial Temperature  = " << Te << endl;
-//t2=clock()-t1;
+  cerr << "\n[ii]  Temperature Initiale = " << Te << endl;
 
 
-//cout<<"timesec"<<(value_type )t2/CLOCKS_PER_SEC << endl;
-/*cerr<<Tab[6][6]<<endl; 
-cerr<<Tab[7][6]<<endl; 
-cerr<<Tab[8][6]<<endl; 
-value_type at={8.97e-9*pow(Te,-1)*exp(-10.68/Te)};
-cerr<<at<<endl;
+
+/* 
+//verification du coefficient d'arrhenius et de la temperature 
 for (int j=0;j<jmax;j++)
 {
  p2=Tab[1][j];
@@ -435,16 +575,9 @@ cerr<<j<<'\t'<<Tp<<'\t'<<Kt[j]<<endl;
 
 cerr<<k(44,Te)<<endl;
 cerr<<k45(Te)<<endl;
+*/
 
-for (int j=0;j<jmax;j++)
-{p2=Tab[1][j];
- g3=Tab[4][j];
- Tp=(p2==0 or g3==0)?Te:Tg;
- Kt[j]={Tab[6][j]*pow(Tp,Tab[7][j])*exp(-Tab[8][j]/Tp)};
- cerr<<j<<'\t'<<Tp<<'\t'<<Kt[j]<<endl;
-}*/
-
-  // declare system and jacobian
+  // declare system et jacobian
   nsystem sys;
   jacobian jac;
 
@@ -453,34 +586,35 @@ for (int j=0;j<jmax;j++)
 
   for (int i = 1; i <= NT+1 ; i++)
   {
-
+//calcul des coefficients pour la diffusion ambipolaire
 value_type n_mu= n_new[0]*mu[0] + n_new[4]*mu[4] + n_new[10]*mu[10] + n_new[20]*mu[20];
 value_type n_DL= -n_new[0]*DL[0] + n_new[4]*DL[4] + n_new[10]*DL[10] + n_new[20]*DL[20];
-
+if (i==NT){cerr<<C<<endl;}
+//diffusion ambipolaire
 DA[0]=(DL[0]+mu[0]*n_DL/n_mu)*diff; //s-1
 DA[1]=DL[1]*diff;
 DA[4]=(DL[4]-mu[4]*n_DL/n_mu)*diff;
 DA[10]=(DL[10]-mu[10]*n_DL/n_mu)*diff;
 DA[20]=(DL[20]-mu[20]*n_DL/n_mu)*diff;
-    // update Te in system and jacobian
+    
+// update Te dans system et jacobian
     sys.Te = Te;
     jac.Te = Te;
-//cerr<<"patate"<<endl;
-    // Integrate at least one step dt
+
+// integration au pas dt
     stepper.do_step( std::make_pair( sys, jac ), n_new, t, dt, n_err);
-//cerr<<"patate2"<<endl;
-    // assign values to functor etemp
+
+    // assignation des valeur a la fonction etemp
     etemp.n = n_new;
-    if (i%((int)(NT/50))==0)
+    if (i%((int)(NT/100))==0)
     {
       write_density(t, Te, n_new);
     }
-    // Find new Te
+    // trouver un noyuveau Te
     pair<value_type, value_type> pair_Te =\
                   toms748_solve(etemp, min, max, tol, max_iter);
-//cerr<<"patate3"<<endl;
-    Te = pair_Te.first;
 
+    Te = pair_Te.first;
     t+= dt;
     n_ini = n_new;//update
   }
